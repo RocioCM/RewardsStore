45b1902810305545ba426ef25da07fe161401fb8
RoCM <rociocmena@gmail.com>
2020-11-21 22:51:36 -0300

    Created Product Services. Implemented in App.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
+import {useEffect, useState} from 'react';
+import ProductsService from './services/productsService';
 import AppProvider from './ContextProvider';
 import HeaderBar from './components/header/HeaderBar';
 import SectionHeader from './components/header/SectionHeader';
-import {useState} from 'react';
+import Gallery from './components/gallery/Gallery';
 import './styles/app.css';
 
 function App() {
 
 	const [section, setSection] = useState({section:"electronics", title:"Electrónica"});
-	
+	const [products, setProducts] = useState([]);
+
+	const getProducts = async () => {
+		let products = null;
+		switch (section.section) {
+			case ('history'):
+				products = await ProductsService.getHistory();
+				break;
+			default:
+				products = await ProductsService.getProducts();
+		};
+		if (products) setProducts(products);
+	}
+
+	useEffect(() => getProducts(),[section]);
+
 	return (
-		<AppProvider context={{setSection}}>
-			<div className='App'>
-				<HeaderBar />
-				<SectionHeader title={section.title} />
-				<h2>Hi, visitor. Showing {section.section==='electronics' ? <i>Electronics section</i> : <i>History section</i>}</h2>
-			</div>
+		<AppProvider context={{products, setSection}}>
+			<HeaderBar />
+			<SectionHeader title={section.title} />
+			<Gallery products={products} />
 		</AppProvider>
 	);
 }
---
+++ src/services/productsService.js
@@ -0,0 +1,56 @@
+class ProductsService {
+	static headers = {
+		"Content-Type": "application/json",
+		"Accept": "application/json",
+		"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmFjMjQ0OWI5NTIzZTAwMjA3ZTFmYzMiLCJpYXQiOjE2MDUxMTcwMDF9.vHMYlEKnpSVDSejVVyittmqUhIQ9pbD2U5CvqwQYJ4I",
+	};
+
+	static async getProducts() {
+		const headers = this.headers;
+		try {
+			let resp = await fetch(
+				"https://coding-challenge-api.aerolab.co/products", {headers});
+			resp = await resp.json();
+			console.log(resp); ///
+			return resp;
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	static async getHistory() {
+		const headers = this.headers;
+		try {
+			let resp = await fetch(
+				"https://coding-challenge-api.aerolab.co/user/history", {headers});
+			resp = await resp.json();
+			console.log(resp); ///
+			return resp;
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	static async addCoins(productId) {
+		const params = {
+			method: 'post',
+			mode: 'cors',
+			headers: this.headers, 
+			body: {
+				productId
+			}
+		};
+		console.log(params); ///
+		try {
+			let resp = await fetch( 
+				"https://coding-challenge-api.aerolab.co/redeem", params);
+			console.log(resp); ///
+			resp = await resp.json();
+			return resp;
+		} catch (error) {
+			console.log(error);
+		}
+	}
+}
+
+export default ProductsService;
